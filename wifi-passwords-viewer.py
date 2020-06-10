fb49311f9a5cab293cad4a1f4559c4af98cf0677
defsec16 <64437509+defsec16@users.noreply.github.com>
2020-06-11 00:40:51 +0600

    Add files via upload

---
+++ wifi-passwords-viewer.py
@@ -0,0 +1,11 @@
+import subprocess
+data = subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8').split('\n')
+profiles = [i.split(":")[1][1:-1] for i in data if "All User Profiles" in i]
+for i in profiles:
+	result = subprocess.check_output(['netsh','wlan','show','profiles', i, 'key=clear']).decode('utf-8').split('\n')
+	result = [b.split(":")[1][1:-1] for b in result if "Key Content" in b]
+	try:
+		print("{:<30}|  {:<}".format(i, result[0]))
+	except IndexError:
+		print("{:<30}|  {:<}".format(i, "")
+input("")
